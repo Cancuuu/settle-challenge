76d8448a454cf7b7b8eac9627e9502e4be29ed6b
Cancu <juancruz.cancu@gmail.com>
2022-02-14 17:41:47 -0300

    divided orders mejorado

--- components/OrderBook.jsx
+++ components/OrderBook.jsx
@@ -6,46 +6,29 @@ import OrderCard from "../components/OrderCard";
 const OrderBook = () => {
   const { tradeOrders } = useContext(StateContext);
 
-  const [buyOrders, setBuyOrders] = useState([]);
-  const [sellOrders, setSellOrders] = useState([]);
-
-  useEffect(() => {
-    tradeOrders.map((order) => {
-      order.operation === "buy"
-        ? setBuyOrders([...buyOrders, order])
-        : setSellOrders([...sellOrders, order]);
-    });
-  }, [tradeOrders]);
 
   return (
     <div className="col-start-1 col-end-4 grid grid-rows-2 h-screen">
       <div className="overflow-auto">
         <h3 className="text-bold text-center text-2xl m-4">BUY</h3>
-        {buyOrders.map((order) => (
-          <OrderCard order={order} key={order.id} />
-        ))}
+        {tradeOrders.map((order) => {
+          if (order.operation === "buy") {
+            return <OrderCard order={order} key={order.id} />;
+          }
+          return null;
+        })}
       </div>
       <div className="overflow-auto">
         <h3 className="text-bold text-center text-2xl m-4">SELL</h3>
-        {sellOrders.map((order) => (
-          <OrderCard order={order} key={order.id} />
-        ))}
+        {tradeOrders.map((order) => {
+          if (order.operation === "sell") {
+            return <OrderCard order={order} key={order.id} />;
+          }
+          return null;
+        })}
       </div>
     </div>
   );
 };
 
 export default OrderBook;
-
-// {tradeOrders.map((order) => {
-//   if (order.operation === "buy") {
-//     return <OrderCard order={order} key={order.id} />;
-//   }
-//   return null;
-// })}
-// {tradeOrders.map((order) => {
-//   if (order.operation === "sell") {
-//     return <OrderCard order={order} key={order.id} />;
-//   }
-//   return null;
-// })}
