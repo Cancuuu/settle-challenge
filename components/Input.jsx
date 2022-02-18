d60d203f99471294e15bb210c56a6981ecc67a85
Cancu <juancruz.cancu@gmail.com>
2022-02-18 17:11:54 -0300

    remove tw

--- components/Input.jsx
+++ components/Input.jsx
@@ -4,10 +4,9 @@ const Input = ({ disabled, placeholder, value, onAction }) => {
   return (
     <input
       disabled={disabled}
-      className="input"
       type="number"
       placeholder={placeholder}
-      className="w-full h-16 text-xl border-2 border-gray-400 rounded-xl p-2"
+      className="input w-full h-16 text-xl border-2 border-gray-400 rounded-xl p-2"
       onChange={(e) => onAction(e.target.value)}
       value={value}
     />
